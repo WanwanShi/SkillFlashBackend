2a0f3ea79e240249f92957dc839db26df473a512
湾仔 <Wendy@shiwanwandembp.home>
2024-05-21 14:12:17 +0100

    pre-deployment setting

--- src/listen.ts
+++ src/listen.ts
@@ -3,6 +3,12 @@ const { PORT = 9090 } = process.env;
 import { connectDB } from "./database/connection";
 
 connectDB()
-app.listen(PORT, () => {
-	console.log("Listening on 9090");
-});
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error("Failed to connect to the database", error);
+		process.exit(1);
+	});
